974aa07f0028ccfe687a5f7be1b403e69945e1b1
ZinoChan <devzinochan@gmail.com>
2023-04-13 18:29:23 +0000

    Add utility functions to shell.h for string manipulation and memory management

--- shell.h
+++ shell.h
@@ -1,5 +1,17 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+char *my_strcpy(char *dest, const char *src);
+char **split_string(char *str, const char *str_separator, int *num_of_words);
+char* read_input();
+size_t get_strlen(const char *str);
+void free_words(char **words, int num_of_words);
 
 #endif
