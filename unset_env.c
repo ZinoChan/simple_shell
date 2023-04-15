e4708703002deaa584679333ee04bfefb85cee1f
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-15 15:09:50 -0700

    Add unset_env function to unset an environment variable

--- unset_env.c
+++ unset_env.c
@@ -1,1 +1,26 @@
 #include "shell.h"
+
+/**
+ * my_unsetenv - Unset an environment variable.
+ *
+ * @env_name: The name of the environment variable to unset.
+ *
+ * Return: 0 on success, -1 on failure.
+ */
+int my_unsetenv(const char *env_name)
+{
+	int final_result;
+
+	if (!env_name || env_name[0] == '\0')
+	{
+		perror("unsetenv: invalid env name\n");
+		return (-1);
+	}
+
+	final_result = unsetenv(env_name);
+
+	if (final_result < 0)
+		perror("unsetenv: failed to unset env\n");
+
+	return (final_result);
+}
