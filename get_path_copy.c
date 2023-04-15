00fff319905a361d56179b958eb6f64e8f92f43b
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-14 20:33:40 -0700

    Add get_path_copy function to gets a copy of the PATH environment variable

--- get_path_copy.c
+++ get_path_copy.c
@@ -1,1 +1,22 @@
 #include "shell.h"
+
+/**
+ * get_path_copy - Gets a copy of the PATH environment variable
+ *
+ * Return: A new allocated string containing a copy of the PATH variable,
+ * or NULL if malloc fails or PATH is not set
+ */
+char *get_path_copy()
+{
+	char *path = getenv("PATH");
+	char *path_copy;
+
+	if (!path)
+		return (NULL);
+
+	path_copy = my_strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+	return (path_copy);
+}
