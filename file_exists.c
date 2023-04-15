ed8fab2c00b177561ee7465b11a09ae0a6ee6077
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-14 20:11:08 -0700

    Add file_exists function to checks if a file exists

--- file_exists.c
+++ file_exists.c
@@ -1,1 +1,16 @@
 #include "shell.h"
+
+/**
+ * file_exists - Checks if a file exists
+ * @path: The path of the file to check
+ *
+ * Return: 1 if the file exists, 0 otherwise
+ */
+int file_exists(const char *path)
+{
+	struct stat buffer;
+
+	if (stat(path, &buffer) != 0)
+		return (0);
+	return (1);
+}
