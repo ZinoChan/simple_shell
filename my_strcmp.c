556d96e7ca33d21b8aa3beee0a2947abcf78e7a6
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-15 15:04:33 -0700

    Add my_strcmp function to compares two strings

--- my_strcmp.c
+++ my_strcmp.c
@@ -1,1 +1,20 @@
 #include "shell.h"
+
+/**
+ * my_strcmp - Compares two strings.
+ *
+ * @first: The first string to be compared.
+ * @second: The second string to be compared.
+ *
+ * Return: An integer less than, equal to, or greater than zero
+ */
+int my_strcmp(const char *first, const char *second)
+{
+	while (*first == *second && *first != '\0')
+	{
+		first++;
+		second++;
+	}
+
+	return ((int)(*first) - (int)(*second));
+}
