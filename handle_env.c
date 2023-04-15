79889fa23d7290aa46646c5643574acb480ecb58
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-15 15:00:58 -0700

    Add handle_env function to handle printing environment variables

--- handle_env.c
+++ handle_env.c
@@ -1,1 +1,16 @@
 #include "shell.h"
+
+/**
+ * handle_the_env - Prints the environment variables.
+ */
+void handle_the_env(void)
+{
+	extern char **environ;
+	char **env_pointer = environ;
+
+	while (*env_pointer)
+	{
+		printf("%s\n", *env_pointer);
+		env_pointer++;
+	}
+}
