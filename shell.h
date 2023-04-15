d3dddf489172d700f47bf7c1aea204fa06118df8
emaoumaima <sellouane.oumaima@gmail.com>
2023-04-14 22:08:07 -0700

    Add function declarations to shell.h header file

--- shell.h
+++ shell.h
@@ -14,10 +14,19 @@ char *read_input();
 size_t get_strlen(const char *str);
 void free_words(char **words, int num_of_words);
 
+/* File management */
+int file_exists(const char *path);
+char *get_dir_path(const char *path, const char *cmd);
+char *get_path_copy();
+
 /*forking*/
 void execute_by_forking(char **arr_of_words);
 char *get_full_path(const char *cmd);
 void execute_cmd_with_execve(char *cmd, char **arr_of_words);
 void wait_kid_process(pid_t pid, int *status);
+
+/* String manipulation */
+char *my_strcat(char *dest, const char *src);
+char *my_strdup(const char *s);
 
 #endif
