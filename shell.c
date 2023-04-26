09beba92614d13b0d2d70767bbed431daad1fe2d
ZinoChan <devzinochan@gmail.com>
2023-04-26 08:08:22 +0000

    handle non_interactive mode

--- shell.c
+++ shell.c
@@ -2,16 +2,33 @@
 
 /**
  * main - Entry point for shell.
- *
+ *@argc: arg number
+ * @argv: argumens
  * Return: Always returns 0.
  */
 
 
-int main(void)
+int main(int argc, char **argv)
 {
 	char *input_holder = NULL;
+	(void)argv;
 
-	while (1)
+	if (argc == 1)
+	{
+		while (1)
+		{
+			input_holder = read_input();
+			if (input_holder == NULL)
+			{
+				printf("\n");
+				return (-1);
+			}
+			exec_multi_cmds(input_holder);
+			free(input_holder);
+			input_holder = NULL;
+		}
+	}
+	else
 	{
 		input_holder = read_input();
 		if (input_holder == NULL)
